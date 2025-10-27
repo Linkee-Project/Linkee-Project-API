35221e283ca93090c7b979642eca2d1c15926271
yendlessse <yoohanse@naver.com>
2025-10-27 17:25:17 +0900

    feat: 기본세팅

---
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/LinkeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkee.linkeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkeeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkeeApiApplication.class, args);
+    }
+
+}
