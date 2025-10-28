4f9181282a3b9ef30371fd974b1ac4816c142967
steamjin <kkjj2024@naver.com>
2025-10-28 23:26:13 +0900

    feat: room_question entity 구현완료

---
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/quiz/model/entity/RoomQuestion.java
@@ -0,0 +1,30 @@
+package com.linkee.linkeeapi.quiz.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_room_question")
+public class RoomQuestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_question_id")
+    private Long roomQuestionId;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_room_id", nullable = false)
+    private QuizRoom quizRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @Column(name = "quiz_order", nullable = false)
+    private Integer quizOrder;
+}
