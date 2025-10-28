8c4fd81a1db653eba3715b22440c95348ed23ad8
yendlessse <yoohanse@naver.com>
2025-10-28 15:49:50 +0900

    feat: inquiry entity 구현 완료

---
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/user/model/entity/User.java
@@ -0,0 +1,50 @@
+package com.linkee.linkeeapi.user.model.entity;
+
+import com.linkee.linkeeapi.common.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "tb_user",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_user_login_id", columnNames = "user_login_id"),
+                @UniqueConstraint(name = "uk_user_nickname", columnNames = "user_nickname")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "user_login_id", nullable = false, length = 50)
+    private String userLoginId;
+
+    @Column(name = "user_pw", nullable = false, length = 300)
+    private String userPw;
+
+    @Column(name = "user_nickname", nullable = false, length = 10)
+    private String userNickname;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_status", nullable = false, columnDefinition = "ENUM('Y','N') DEFAULT 'Y'")
+    private Status userStatus = Status.Y;
+}
