8253e73af41307f4a64af91de0865dd7a711bbbb
dzmajin <qpalzm3415@gmail.com>
2025-11-02 16:22:25 +0900

    feat : create 조회 구현 중...

--- Linkee-api/src/main/java/com/linkee/linkeeapi/question/command/application/dto/request/CreateQuestionRequestDto.java
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/question/command/application/dto/request/CreateQuestionRequestDto.java
@@ -1,4 +1,29 @@
 package com.linkee.linkeeapi.question.command.application.dto.request;
 
+import com.linkee.linkeeapi.user.command.domain.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class CreateQuestionRequestDto {
+
+    @NotBlank
+    private Long userId; //작성자 ID
+
+    @NotNull
+    private Long categoryId;
+
+    @NotBlank
+    @Size(max = 100)
+    private String questionTitle;
+
+    @NotBlank
+    private String questionQuestion;
+
+    @NotNull
+    private Integer questionAnswer;
 }
--- Linkee-api/src/main/java/com/linkee/linkeeapi/question/command/application/service/QuestionCommandServiceImpl.java
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/question/command/application/service/QuestionCommandServiceImpl.java
@@ -1,19 +1,43 @@
 package com.linkee.linkeeapi.question.command.application.service;
 
+import com.linkee.linkeeapi.category.command.aggregate.Category;
+import com.linkee.linkeeapi.category.command.infrastructure.repository.CategoryRepository;
+import com.linkee.linkeeapi.common.enums.Status;
 import com.linkee.linkeeapi.question.command.application.dto.request.CreateQuestionRequestDto;
+import com.linkee.linkeeapi.question.command.domain.aggregate.Question;
 import com.linkee.linkeeapi.question.command.infrastructure.repository.JpaQuestionRepository;
+import com.linkee.linkeeapi.user.command.application.service.util.UserFinder;
+import com.linkee.linkeeapi.user.command.domain.entity.User;
+import com.linkee.linkeeapi.user.command.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class QuestionCommandServiceImpl implements QuestionCommandService {
 
     private final JpaQuestionRepository jpaQuestionRepository;
+    private final UserFinder userFinder;
+    private final CategoryRepository categoryRepository;
 
+
+    @Transactional
     public void createQuestion(CreateQuestionRequestDto request) {
+
+        Category category = categoryRepository.findById(request.getCategoryId())
+                .orElseThrow(()-> new IllegalArgumentException("카테고리를 찾을 수 없습니다."));
+        Question question = Question.builder()
+                .category(category)
+                .questionTitle(request.getQuestionTitle())
+                .questionQuestion(request.getQuestionQuestion())
+                .questionAnswer(request.getQuestionAnswer())
+                .user(userFinder.getById(request.getUserId()))
+                .isQualified(Status.N)
+                .isDeleted(Status.N)
+                .questionViews(0L)
+                .build();
+        jpaQuestionRepository.save(question);
 
     }
 }
