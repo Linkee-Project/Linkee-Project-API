6c3843e683febdac00140e40a0e5d7154f5dfe7d
steamjin <kkjj2024@naver.com>
2025-11-04 16:52:26 +0900

    feat: quiz_current_index cud 구현완료 // quiz_room 시작 기능 구현 완료

--- Linkee-api/src/main/java/com/linkee/linkeeapi/room_member/command/infrastructure/repository/RoomMemberRepository.java
+++ Linkee-api/src/main/java/com/linkee/linkeeapi/room_member/command/infrastructure/repository/RoomMemberRepository.java
@@ -1,7 +1,12 @@
 package com.linkee.linkeeapi.room_member.command.infrastructure.repository;
 
+import com.linkee.linkeeapi.quiz_room.command.domain.aggregate.QuizRoom;
 import com.linkee.linkeeapi.room_member.command.domain.aggregate.RoomMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomMemberRepository extends JpaRepository<RoomMember, Long> {
+    // 특정 QuizRoom에 속한 모든 RoomMember를 조회하는 메서드
+    List<RoomMember> findByQuizRoom(QuizRoom quizRoom);
 }
